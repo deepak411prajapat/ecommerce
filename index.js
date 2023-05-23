8acac96d360157e930501a4f920675ab378f3b95
deepak411prajapat <94034668+deepak411prajapat@users.noreply.github.com>
2023-05-23 12:09:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,74 +1,20 @@
-import React, {Component} from "react";
-import ReactDOM from "react-dom";
-//import "./style.css";
-import questionAPI from './question';
-import QuestionBox from './components/QuestionBox';
-import Result from './components/ResultBox';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+import {ProductProvider} from './context';
+import * as serviceWorker from './serviceWorker';
 
-class Quiz extends Component {
-constructor() {
-	super();
-	this.state = {
-	questionBank: [],
-	score: 0,
-	responses: 0
-	};
-}
+ReactDOM.render(
+    <ProductProvider>
+        <Router>
+            <App />
+        </Router>
+    </ProductProvider>,
+     document.getElementById('root'));
 
-// Function to get question from ./question
-getQuestions = () => {
-	questionAPI().then(question => {
-	this.setState({questionBank: question});
-	});
-};
-
-// Set state back to default and call function
-playAgain = () => {
-	this.getQuestions();
-	this.setState({score: 0, responses: 0});
-};
-
-// Function to compute scores
-computeAnswer = (answer, correctAns) => {
-	if (answer === correctAns) {
-	this.setState({
-		score: this.state.score + 1
-	});
-	}
-	this.setState({
-	responses: this.state.responses < 5
-		? this.state.responses + 1
-		: 5
-	});
-};
-
-// componentDidMount function to get question
-componentDidMount() {
-	this.getQuestions();
-}
-
-render() {
-	return (<div className="container">
-	<div className="title">
-		QuizOn
-	</div>
-
-	{this.state.questionBank.length > 0 &&
-	this.state.responses < 5 &&
-	this.state.questionBank.map(({question, answers,
-	correct, questionId}) => <QuestionBox question=
-	{question} options={answers} key={questionId}
-	selected={answer => this.computeAnswer(answer, correct)}/>)}
-
-	{
-		this.state.responses === 5
-		? (<Result score={this.state.score}
-			playAgain={this.playAgain}/>)
-		: null
-	}
-
-	</div>)
-}
-}
-
-ReactDOM.render(<Quiz/>, document.getElementById("root"));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
